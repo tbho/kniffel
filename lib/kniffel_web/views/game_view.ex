e1fe884fce6ca26f18cbeb49cea23adfd212cd0e
Tobias Hoge <tbho@tbho.de>
2019-12-10 18:26:40 +0100

    add score calculation

--- lib/kniffel_web/views/game_view.ex
+++ lib/kniffel_web/views/game_view.ex
@@ -1,7 +1,7 @@
 defmodule KniffelWeb.GameView do
   use KniffelWeb, :view
 
-  def display_score(scores, user_id, score_type) do
+  def score_exists?(scores, user_id, score_type) do
     Enum.find(scores, fn score ->
       score.user_id == user_id && score.score_type == score_type
     end)
@@ -53,5 +53,110 @@ defmodule KniffelWeb.GameView do
             {:none, dice}
         end
     end
+  end
+
+  def calculate_score(roll, type) do
+    case type do
+      :aces ->
+        calculate_number(roll, 1)
+
+      :twos ->
+        calculate_number(roll, 2)
+
+      :threes ->
+        calculate_number(roll, 3)
+
+      :fours ->
+        calculate_number(roll, 4)
+
+      :fives ->
+        calculate_number(roll, 5)
+
+      :sixes ->
+        calculate_number(roll, 6)
+
+      :three_of_a_kind ->
+        result = count_occurences(roll)
+
+        if 3 in Map.values(result) || 4 in Map.values(result) || 5 in Map.values(result) do
+          calculate_roll(roll)
+        else
+          0
+        end
+
+      :four_of_a_kind ->
+        result = count_occurences(roll)
+
+        if 4 in Map.values(result) || 5 in Map.values(result) do
+          calculate_roll(roll)
+        else
+          0
+        end
+
+      :full_house ->
+        result = count_occurences(roll)
+
+        if 3 in Map.values(result) && 2 in Map.values(result) do
+          25
+        else
+          0
+        end
+
+      :small_straight ->
+        if Enum.all?([1, 2, 3, 4], fn x -> contains_number?(roll, x) end) ||
+             Enum.all?([2, 3, 4, 5], fn x -> contains_number?(roll, x) end) ||
+             Enum.all?([3, 4, 5, 6], fn x -> contains_number?(roll, x) end) do
+          30
+        else
+          0
+        end
+
+      :large_straight ->
+        if Enum.all?([1, 2, 3, 4, 5], fn x -> contains_number?(roll, x) end) ||
+             Enum.all?([2, 3, 4, 5, 6], fn x -> contains_number?(roll, x) end) do
+          40
+        else
+          0
+        end
+
+      :kniffel ->
+        result = count_occurences(roll)
+
+        if 5 in Map.values(result) do
+          50
+        else
+          0
+        end
+
+      :chance ->
+        calculate_roll(roll)
+    end
+  end
+
+  def calculate_number(roll, number) do
+    Enum.reduce(roll, 0, fn {_, x}, result ->
+      if x == number do
+        result + x
+      else
+        result
+      end
+    end)
+  end
+
+  def calculate_roll(roll) do
+    Enum.reduce(roll, 0, fn {_type, x}, result ->
+      result + x
+    end)
+  end
+
+  def contains_number?(roll, number) do
+    Enum.any?(roll, fn {_type, x} -> x == number end)
+  end
+
+  def count_occurences(roll) do
+    Enum.reduce(roll, %{}, fn {_type, x}, result ->
+      temp = Map.get(result, x, 0)
+      Map.put(result, x, temp + 1)
+    end)
   end
 end
