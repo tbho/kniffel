71cd6ab27b743d12efbf86c98e62f126408c09ae
Tobias Hoge <tbho@tbho.de>
2020-01-02 23:12:20 +0100

    add logger

--- lib/kniffel/request.ex
+++ lib/kniffel/request.ex
@@ -23,13 +23,13 @@ defmodule Kniffel.Request do
       {:ok, response_body}
     else
       {:request, {:error, message}} ->
-        {:ok, %{body: body} = response
+        {:ok, %{body: body}} = response
         Logger.debug(message)
         Logger.debug(body)
         {:error, "request_failed"}
 
       {:status_code, status_code} ->
-        {:ok, %{body: body} = response
+        {:ok, %{body: body}} = response
         Logger.debug(status_code)
         Logger.debug(body)
         {:error, "status_code"}
@@ -38,7 +38,7 @@ defmodule Kniffel.Request do
       #   {:error, "wrong_content_type"}
 
       {:json, {:error, message}} ->
-        {:ok, %{body: body} = response
+        {:ok, %{body: body}} = response
         Logger.debug(message)
         Logger.debug(body)
         {:error, "decode_failed"}
