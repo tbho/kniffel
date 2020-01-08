67428f7fb18067cccdc45d86098f96612542cfaf
Tobias Hoge <tbho@tbho.de>
2020-01-08 20:39:42 +0100

    fix transaction signature error

--- lib/kniffel/server/server.ex
+++ lib/kniffel/server/server.ex
@@ -189,7 +189,7 @@ defmodule Kniffel.Server do
         end)
         |> Map.new()
 
-      timestamp = Timex.now() |> Timex.format!("{ISO:Extended}")
+      timestamp = Timex.now() |> DateTime.truncate(:second) |> Timex.format!("{ISO:Extended}")
 
       signature =
         Poison.encode!(%{"dices" => dices, "timestamp" => timestamp}) |> IO.inspect
